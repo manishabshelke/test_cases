0d782df2784da900b4ee37803c4fd800988da737
manishabshelke <64117536+manishabshelke@users.noreply.github.com>
2021-04-13 21:47:32 +0530

    new changes

--- src/test/java/com/qa/testcases/OpenAccountTest.java
+++ src/test/java/com/qa/testcases/OpenAccountTest.java
@@ -1,5 +1,6 @@
 package com.qa.testcases;
 
+import org.openqa.selenium.By;
 import org.testng.annotations.Test;
 
 import com.qa.testBase.TestBase;
@@ -7,18 +8,11 @@ import com.qa.testBase.TestBase;
 public class OpenAccountTest extends TestBase {
 	
 	@Test
-	public void  openAccountTest(Hashtable<String,String>data) throws InterruptedException{
-		click("openacc_CSS");
-		select("customer_CSS", data.get("customer"));
+	public void  openAccountTest(String customer, String currency ) throws InterruptedException{
+		driver.findElement(By.xpath(OR.getProperty("BankManagerLogin"))).click();
+		
 		Thread.sleep(1000);
-		select("currency_CSS", data.get("currency"));
-		Thread.sleep(1000);
-		click("process_CSS");
-		Thread.sleep(2000);
-		Alert alert= wait.until(ExpectedConditions.alertIsPresent());
-			//Assert.assertTrue(alert.getText().contains(alerttext));
-			
-		alert.accept();
+
 		
 	}
 
